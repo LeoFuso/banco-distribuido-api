b562201f568ac91972ceee9bc8a60d4a48782169
Leonardo Fuso Nuzzo <leonardofusonuzzo@gmail.com>
2019-04-23 13:38:48 -0300

    dev - Renomeação da classe Main

--- src/test/java/com/leofuso/academico/cd/bancod/BancoDistribuidoServerApplicationTests.java
+++ src/test/java/com/leofuso/academico/cd/bancod/BancoDistribuidoServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BancoDistribuidoApplicationTests {
+public class BancoDistribuidoServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
